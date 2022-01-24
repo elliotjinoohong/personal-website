41b23390b0ee5820825cfab3d39dbe508e7dcbc6
elliotjinoohong <elliot_hong@brown.edu>
2022-01-24 03:19:59 -0500

    set up env and up to min 8 in video

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Personal-Website
+# Personal Website
+
+Following devaslife: https://www.youtube.com/watch?v=bSMZgXzC9AA&t=4483s&ab_channel=devaslife
---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import Layout from '/..components/layouts/Main'
+
+const Website = ({ Component, pageProps, router} ) => {
+  return (
+    <ChakraProvider>
+      
+    </ChakraProvider>
+  )
+}
